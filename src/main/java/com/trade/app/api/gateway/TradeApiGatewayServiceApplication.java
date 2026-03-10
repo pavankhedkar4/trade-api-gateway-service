1d95202b1ced133ecfebbd32a81e8b258c260350
pavankhedkar4 <pavankhedkar4@gmail.com>
2026-03-10 13:13:23 +0530

    Initial commit

---
+++ src/main/java/com/trade/app/api/gateway/TradeApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trade.app.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeApiGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trade/app/api/gateway/TradeApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trade.app.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
